c8bccd768e5c095f81f921b79177e591b942f5dc
jemiahc <jemiahc@g.clemson.edu>
2020-01-27 15:26:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jemiah/reverseakinator/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.jemiah.reverseakinator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int hidden;
+
+    public void talk(View view){
+
+        Toast.makeText(this, "Please stop touching me.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void generateRandomNumber(){
+        Random rand = new Random();
+        hidden = rand.nextInt(100) + 1;
+    }
+
+    public void guess(View view){
+
+        String message = "";
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        if(editText.getText().toString().isEmpty()){
+            message = "Please enter a number, idiot...";
+        } else {
+            int userGuess = Integer.parseInt(editText.getText().toString());
+
+            if (userGuess > hidden)
+                message = "Lower!";
+
+            else if (userGuess < hidden)
+                message = "Higher!";
+
+            else{
+                message = "Yes, that's it! Again, again!";
+                generateRandomNumber();
+            }
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        Log.i("Info", Integer.toString(hidden));
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateRandomNumber();
+
+    }
+}
